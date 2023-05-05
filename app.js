c6ad09b44904d18b110f9ebd06fc43700db061bd
nelo_olikeze <neloolikeze@gmail.com>
2023-05-05 06:15:03 +0100

    feat: add a 404 error page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({extended : false}))
 app.use(adminRoutes)
 app.use(shopRoutes)
 
+
+app.use((req, res, next) => {
+    res.status(400).send('<h1>Page not found</h1>')
+})
 app.listen(3000,() => {
     console.log('Listening to server on port 3000')
 });
