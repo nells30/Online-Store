b513b501aa5ede3450bb9a3c6701bdaa82818983
nelo_olikeze <neloolikeze@gmail.com>
2023-05-10 04:59:04 +0100

    remove styling from html pages and serve statically

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use(bodyParser.urlencoded({extended : false}))
+app.use(Express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', 'page-error.html'))
